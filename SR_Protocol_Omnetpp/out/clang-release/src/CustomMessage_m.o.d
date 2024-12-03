fd7bd6afae76525ae51c690eda8a61adf3ab8058
Ahmed Osama Helmy <ahmed.osama1982002@gmail.com>
2024-12-03 14:51:33 +0200

    Added examples and created CustomMessage

---
+++ SR_Protocol_Omnetpp/out/clang-release/src/CustomMessage_m.o.d
@@ -0,0 +1,6 @@
+../out/clang-release/src/CustomMessage_m.o: CustomMessage_m.cc \
+  CustomMessage_m.h C:/Setup/omnetpp/include\omnetpp.h
+
+CustomMessage_m.h:
+
+C:/Setup/omnetpp/include\omnetpp.h:
--- SR_Protocol_Omnetpp/out/clang-release/src/Node.o.d
+++ SR_Protocol_Omnetpp/out/clang-release/src/Node.o.d
@@ -1,6 +1,8 @@
 ../out/clang-release/src/Node.o: Node.cc Node.h \
-  C:/Setup/omnetpp/include\omnetpp.h
+  C:/Setup/omnetpp/include\omnetpp.h CustomMessage_m.h
 
 Node.h:
 
 C:/Setup/omnetpp/include\omnetpp.h:
+
+CustomMessage_m.h:
